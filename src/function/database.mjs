b4ce71ad012b1947638ae8fcef89325391c41af4
PhilipHsu609 <39871628+PhilipHsu609@users.noreply.github.com>
2026-07-19 00:46:58 +0800

    Update OverseaVideo host URL in database.mjs

--- src/function/database.mjs
+++ src/function/database.mjs
@@ -2,7 +2,7 @@ export default {
 	Redirect: {
 		Settings: {
 			Host: {
-				OverseaVideo: "upos-sz-mirrorali.bilivideo.com",
+				OverseaVideo: "cn-hk-eq-01-03.bilivideo.com",
 				BStar: "upos-sz-mirrorali.bilivideo.com",
 				PCDN: "upos-sz-mirrorali.bilivideo.com",
 				MCDN: "proxy-tf-all-ws.bilivideo.com",
